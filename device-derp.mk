2c0933bd99c7830d72a3356cf73581221273a6e1
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    redfin: Build PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c

--- device-derp.mk
+++ device-derp.mk
@@ -9,5 +9,8 @@ $(call inherit-product, device/google/redbull/device-derp.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-derp
 
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
+
 # wireless_charger HAL service
 include device/google/redfin/wireless_charger/wireless_charger.mk
