c312b76bdfead6c661a633da1b2b0277b0ed5c99
Michael Bestas <mkbestas@gmail.com>
2022-05-04 20:11:46 +0300

    Convert HbmSVManager overlays to RRO
    Auto generated RRO is not working here because we don't actually have
    access to vendor/google_devices/sunfish/proprietary/display/HbmSVManager
    
    Change-Id: I54d885c233151498372e1b510284d6a297ac3e94

--- device-derp.mk
+++ device-derp.mk
@@ -9,6 +9,10 @@ $(call inherit-product, device/google/redbull/device-derp.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-derp
 
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlay
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
