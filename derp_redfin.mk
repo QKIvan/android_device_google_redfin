75b4eb6d049ddf1a5674db1e8b0468520af2d8dc
QKIvan <ivan33@163.com>
2023-12-01 21:59:51 -0500

    redfin: Set DISABLE_ARTIFACT_PATH_REQUIREMENTS := true

--- derp_redfin.mk
+++ derp_redfin.mk
@@ -6,6 +6,7 @@
 
 # Inherit some common DerpFest stuff.
 TARGET_DISABLE_EPPE := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
